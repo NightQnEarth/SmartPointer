89a4286a99898c24cc4d0d416a95f66fb429fae0
NightQnEarth <Denis1.618@yandex.ru>
2019-04-26 13:01:09 +0500

    Designed SmartPointer interface.

--- SmartPointerLib/SmartPointer.cpp
+++ SmartPointerLib/SmartPointer.cpp
@@ -1,1 +1,55 @@
 #include "SmartPointer.h"
+
+template<typename T>
+SmartPointer<T>::SmartPointer(T* basePointer)
+{
+
+}
+
+template<typename T>
+SmartPointer<T>::~SmartPointer()
+{
+
+}
+
+template<typename T>
+SmartPointer<T>::SmartPointer(const SmartPointer<T>& otherPointer)
+{
+
+}
+
+template<typename T>
+SmartPointer<T>& SmartPointer<T>::operator=(const SmartPointer<T>& otherPointer)
+{
+    return ;
+}
+
+template<typename T>
+T* const SmartPointer<T>::operator->() const
+{
+    return nullptr;
+}
+
+template<typename T>
+T SmartPointer<T>::operator*() const
+{
+    return nullptr;
+}
+
+template<typename T>
+const T& SmartPointer<T>::Get() const
+{
+    return ;
+}
+
+template<typename T>
+void SmartPointer<T>::Set(const T& value)
+{
+
+}
+
+template<typename T>
+void SmartPointer<T>::Release()
+{
+
+}
--- SmartPointerLib/SmartPointer.h
+++ SmartPointerLib/SmartPointer.h
@@ -2,9 +2,23 @@
 #define SMARTPOINTER_SMARTPOINTER_H
 
 
+template <typename T>
 class SmartPointer
 {
+public:
+    SmartPointer(T* basePointer);
+    ~SmartPointer();
+    SmartPointer(const SmartPointer<T>& otherPointer);
+    SmartPointer<T>& operator= (const SmartPointer<T>& otherPointer);
+    T* const operator->() const;
+    T operator*() const;
 
+    const T& Get() const;
+    void Set(const T& value);
+    void Release();
+
+private:
+    T* basePointer;
 };
 
 
